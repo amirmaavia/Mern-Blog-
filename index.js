ff9b31b2a57975a107dfb11ce6d4f473787fccf1
amirmaavia <Maaviyamansoori222@gmail.com>
2021-01-23 05:53:41 +0500

    string clear

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const connect = require('./config/db')
 require('dotenv').config()
 const app = express()
+
+//connect mogodb database
+connect()
 
 app.get('/', (req, res) => {
     res.send(`<html><b>you are in mern blogs</b></html>`)
